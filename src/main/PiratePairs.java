ad0af7867df1cb502fd3537a72cac2f9fefe7396
tuckernichols <139187527+tuckernichols@users.noreply.github.com>
2024-12-06 15:44:00 +0000

    create a static array of all players. started to create the frame of the game, 2 loops one that runs as long as there are player and one that loops thougth the players. lasty i created 2 basic stategies to use in game. always dealt cards and always steal cards.

--- src/main/PiratePairs.java
+++ src/main/PiratePairs.java
@@ -1,13 +1,25 @@
+import java.util.Arrays;
 public class PiratePairs {
     public static void main(String[] args){
         System.out.println("hello world");
         
-        Player peter = new Player("peter");
+        Player peter = new Player("peter");  // always takes a card
         Player tony = new Player("tony");
         Player Stephen = new Player("Stephen");
         Player Bruce = new Player("Bruce");
         Player Bucky = new Player("Bucky");
         Player Thor = new Player("Thor");
+
+        System.out.println(Arrays.toString(Player.players));   // array of all players
+
+        while(Player.players.length > 5){
+            for(Player active : Player.players){
+                
+
+
+            }
+        }
+
 
 
     }  
--- src/main/Player.java
+++ src/main/Player.java
@@ -1,18 +1,34 @@
+import java.util.Arrays;
 public class Player {
     private String name;
-    private int[] cards;
+    private int[] cards = new int[0];
     private int score = 0;
-    public static int numberOfPlayers = 0;
-    public static Player[] = new Player[5];
-
-
+    public static Player[] players = new Player[0];
 
     public Player(String name){
-        numberOfPlayers += 1;
+        appendItemPlayer();
         this.name = name;
     }
 
+    private void appendItemPlayer(){
+        Player[] newArray = new Player[players.length + 1];
+        for(int i = 0; i < players.length; i++){
+            newArray[i] = players[0];
+        }
+        newArray[players.length] = this;
+        players = newArray;
+    }
 
+    private boolean strategy1(){        // always dealt a card
+        return true;
+    }
+
+    private boolean strategy2(){      // always steal a card
+        if(cards.length == 0){
+            return true;
+        }
+        return false;
+    }
 
 
 
